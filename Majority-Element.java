e0384580d283e887f73e4c175b0647999ee04506
Khushal Mishra <129203300+Khushalmishra11@users.noreply.github.com>
2025-05-31 15:13:11 +0530

    [169] [Time Beats: 75.00%] [Memory Beats: 30.14%] - LeetPush

--- Majority-Element.java
+++ Majority-Element.java
@@ -1,4 +1,5 @@
 class Solution {
+  /* hashing approach
     public int majorityElement(int[] nums) {
         int n = nums.length / 2;
         Map<Integer, Integer> map = new HashMap<>();
@@ -11,5 +12,17 @@ class Solution {
             if(map.get(i) > n) return i;
         }
         return 0;
+    } */
+
+    // Boyer moore algorithm
+    public int majorityElement(int[] nums) {
+      int count =0, candidate = 0;
+      for(int num : nums){
+        if(count == 0){
+          candidate = num;
+        }
+          count += (num == candidate) ?1 : -1;
+      }
+      return candidate;
     }
 }
