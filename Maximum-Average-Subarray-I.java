7a732e003387557cff2350cd1f7acc490f553b39
Khushal Mishra <129203300+Khushalmishra11@users.noreply.github.com>
2025-09-17 21:38:12 +0530

    [643] [Time Beats: 64.90%] [Memory Beats: 66.50%] - LeetPush

---
+++ Maximum-Average-Subarray-I.java
@@ -0,0 +1,32 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+       // BRUTE FORCE APPROACH
+       /*
+        double max = Double.NEGATIVE_INFINITY;
+        for(int i = 0; i <= nums.length - k; i++){
+            int sum = 0;
+            for(int j = i; j < i+k ; j++){
+                sum = sum + nums[j];
+            }
+            max = Math.max(max,(double) sum / k);
+
+        }
+        return max;
+
+        */
+
+        // Optimised Approach-  SLIDING WINDOW
+        int sum = 0;
+        for(int i = 0; i < k; i++){
+            sum = sum + nums[i];
+
+
+        }
+        double max = (double)sum/k;
+        for(int i = k; i < nums.length; i++){
+            sum = sum - nums[i-k] + nums[i];
+            max = Math.max(max, (double) sum/k);
+        }
+        return max;
+    }
+}
