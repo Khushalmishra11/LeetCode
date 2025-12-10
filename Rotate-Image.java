80781aad47b9e09ce963340065e5a909a05e4882
Khushal Mishra <129203300+Khushalmishra11@users.noreply.github.com>
2025-12-10 20:50:25 +0530

    [48] [Time Beats: 100.00%] [Memory Beats: 48.35%] - LeetPush

--- Rotate-Image.java
+++ Rotate-Image.java
@@ -1,28 +1,29 @@
 1class Solution {
-2    private void swap(int[][] matrix, int i , int j){
-3        int temp = matrix[i][j];
-4        matrix[i][j] = matrix[j][i];
-5        matrix[j][i]= temp;
-6    }
-7    private void reverse(int[][] matrix, int i){
-8        int left = 0 , right = matrix.length-1;
-9        while(left <= right){
-10            int temp = matrix[i][left];
-11            matrix[i][left] = matrix[i][right];
-12            matrix[i][right] = temp;
-13            left++;
-14            right--;
-15        }
-16    }
-17    public void rotate(int[][] matrix) {
-18        int size = matrix.length;
-19        for(int i = 0; i < size; i++){
-20            for(int j = i+1; j<size; j++){
-21                swap(matrix, i , j);
-22            }
-23        }
-24        for(int i = 0; i < size; i++){
-25            reverse(matrix, i);
-26        }
-27    }
-28}
+2    public void rotate(int[][] matrix) {
+3        int size = matrix.length;
+4        for(int i = 0; i < size; i++){
+5            for(int j = i+1; j < size; j++){
+6                swap(matrix, i ,j);
+7            }
+8        }
+9        for(int i = 0; i < size; i++){
+10            reverse(matrix, i);
+11        }
+12    }
+13    private void swap(int[][] matrix, int i , int j){
+14        int temp = matrix[i][j];
+15        matrix[i][j] = matrix[j][i];
+16        matrix[j][i] = temp;
+17    }
+18    private void reverse(int[][] matrix, int i){
+19        int left = 0, right = matrix.length - 1;
+20
+21        while(left<= right){
+22            int temp = matrix[i][left];
+23            matrix[i][left] = matrix[i][right];
+24            matrix[i][right] = temp;
+25            left++;
+26            right--;
+27        }
+28    }
+29}
